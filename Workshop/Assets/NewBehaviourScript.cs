43109f88dcc364e1bb0e58e2b5a0b3cb42a4eba2
jedklika <43482414+jedklika@users.noreply.github.com>
2021-09-24 17:53:13 -0400

    Revert "Revert message"
    This reverts commit 238c0d9cd590ac461663add641cb8ef86203166b.

--- Workshop/Assets/NewBehaviourScript.cs
+++ Workshop/Assets/NewBehaviourScript.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Time for reversions");
+        Debug.Log("Github is a bit tricky");
 
     }
 
